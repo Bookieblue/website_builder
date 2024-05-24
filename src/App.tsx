03ddcc4e1afc2a287df369924b8838ce156033ec
Bookieblue <oluwabukola.fasan@gmail.com>
2024-05-24 19:50:18 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <section className="max-container padding-container w-full flex-col flexCenter">
       <h1 className="font-bold my-5 text-4xl text-[#353fa9]">
-         Web-Calumaths
+         Web-Calmaths
       </h1>
       <div className="flexBetween flex-col lg:flex-row  w-full">
      <Hero />
